fbc593a0f82acad9cd9a7f1bb0fec4ae6fa7164e
sakshi <sakshisrivatsa04@gmail.com>
2025-07-31 18:49:43 +0530

    use memo

--- src/App.js
+++ src/App.js
@@ -1,104 +1,38 @@
-import React, { useReducer, useState } from 'react';
- 
-// Initial state
-const initialState = [];
- 
-// Reducer function
-function userReducer(state, action) {
-  switch (action.type) {
-    case 'ADD_USER':
-      return [...state, action.payload];
-    case 'UPDATE_USER':
-      return state.map((user) =>
-        user.id === action.payload.id ? action.payload : user
-      );
-    case 'DELETE_USER':
-      return state.filter((user) => user.id !== action.payload);
-    default:
-      return state;
-  }
-}
- 
+import logo from './logo.svg';
+import './App.css';
+import Login from './Login'
+import Garage from './Garage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import NoPage from './NoPage';
+import Timer from './Timer';
+import Counter from './Counter';
+import TutorialsListComponent from './tutorials-list.component';
+import AddTutorial from './AddTutorial';
+import Tutorial from './Tutorial';
+import CounterUsingRedux from './CounterUsingRedux';
+import AppUsingReducer from './AppUsingReducer';
+import UseMemoExample from './UseMemoExample';
+import TutorialsComponent from './TutorialsComponent';
+import ExpensiveCalculator from './ExpensiveCalculator';
+import Parent from './Parent';
 function App() {
-  const [state, dispatch] = useReducer(userReducer, initialState);
- 
-  const [form, setForm] = useState({ id: '', name: '', email: '' });
-  const [isEditMode, setIsEditMode] = useState(false);
- 
-  const handleChange = (e) => {
-    setForm({ ...form, [e.target.name]: e.target.value });
-  };
- 
-  const handleSubmit = (e) => {
-    e.preventDefault();
- 
-    if (isEditMode) {
-      dispatch({ type: 'UPDATE_USER', payload: form });
-      setIsEditMode(false);
-    } else {
-      const newUser = {
-        ...form,
-        id: Date.now(), // generate unique id
-      };
-      dispatch({ type: 'ADD_USER', payload: newUser });
-    }
- 
-    setForm({ id: '', name: '', email: '' });
-  };
- 
-  const handleEdit = (user) => {
-    setForm(user);
-    setIsEditMode(true);
-  };
- 
-  const handleDelete = (id) => {
-    dispatch({ type: 'DELETE_USER', payload: id });
-  };
- 
   return (
-<div style={{ padding: 20 }}>
-<h2>React useReducer CRUD Example</h2>
- 
-      <form onSubmit={handleSubmit}>
-<input
-          type="text"
-          name="name"
-          placeholder="Name"
-          value={form.name}
-          onChange={handleChange}
-          required
-        />
-<br />
-<input
-          type="email"
-          name="email"
-          placeholder="Email"
-          value={form.email}
-          onChange={handleChange}
-          required
-        />
-<br />
-<button type="submit">{isEditMode ? 'Update' : 'Add'} User</button>
-</form>
- 
-      <hr />
- 
-      <h3>User List</h3>
-      {state.length === 0 ? (
-<p>No users added.</p>
-      ) : (
-<ul>
-          {state.map((user) => (
-<li key={user.id}>
-<strong>{user.name}</strong> ({user.email})
-<button onClick={() => handleEdit(user)}>Edit</button>
-<button onClick={() => handleDelete(user.id)}>Delete</button>
-</li>
-          ))}
-</ul>
-      )}
-</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ExpensiveCalculator/>}/>
+        <Route index element={<Parent />} />
+        <Route path='/login' element={<Login />} />
+        <Route path="/garage" element={<Garage/>}/>
+        <Route path="/timer" element={<Timer/>}/>
+        <Route path="/counter" element={<Counter/>}/>
+         <Route path="/tutorials" element={<TutorialsListComponent/>}/>
+         <Route path="/tutorials/:id" element={<Tutorial/>} />
+         <Route path="/addTutorials" element={<AddTutorial/>}/>
+         <Route path="/tutorialsComponent" element={<TutorialsComponent />} />
+        <Route path="*" element={<NoPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
- 
+
 export default App;
